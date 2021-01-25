c6d08954c3e931b0d545cd8c6cbaa13d151956ab
gasipa7 <69954877+gasipa7@users.noreply.github.com>
2021-01-25 15:36:25 +1100

    test1
    just checking

---
+++ practice.py
@@ -0,0 +1,9 @@
+for each in range(0, 101):
+    if each % 2 == 0 and each % 5 == 0:
+        print(each)
+        print("FizzBuzz")
+    if each % 2 == 0:
+        print("Fizz")
+    if each % 5 == 0:
+        print("Buzz");
+        
